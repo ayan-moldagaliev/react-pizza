1cd5ee961fbbfa565f3a210a245d4d8100c398c7
ayan-moldagaliev <a9127742@gmail.com>
2025-08-08 13:34:23 +0500

    Implements interactive category selection
    Replaces static category list with a dynamic one that uses React state to manage the active category.
    
    This allows users to select a category and updates the UI to reflect the selection.

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
+
 const Categories = () => {
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleClick = (index) => {
+    setActiveIndex(index);
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((category, index) => (
+          <li onClick={() => handleClick(index)} className={activeIndex === index && 'active'}>{category}</li>
+        ))}
       </ul>
     </div>
   );
